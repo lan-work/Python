b4e7e71c129a4a4a0f2ebc230af1f890e5a0d063
lan-work <d.iv@abv.bg>
2024-06-20 16:26:31 +0300

    Create Random_stuff.py

---
+++ Basics/Random_stuff.py
@@ -0,0 +1,10 @@
+print("//----- Default end='\\n'-----/")
+print(1); print(2); print(3)
+print(1, 2, 3, end='!')
+print('\n\n')
+
+
+print('//----- Duplicate Placeholders -----/')
+print('{1} + {1} * {1} = {0}'.format(3+3*3, 3))
+
+
