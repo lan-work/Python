fc966f29bb61750a2898ba2392b8899467b238cf
lan-work <d.iv@abv.bg>
2024-06-25 22:00:29 +0300

    Create Random_notes.py

---
+++ Basics/Random_notes.py
@@ -0,0 +1,25 @@
+# # Стойност по подразбиране на print(end) е \n
+# print(1)
+# print(2)
+# print(3)
+# print(1, 2, 3, end='!')
+#
+#
+#
+# # Шаблоните(Placeholders) могат да се повтарят
+# print('{1} + {1} * {1} = {0}'.format(3 + 3 * 3, 3))
+#
+#
+#
+# # Степенуване
+# print(2 ** 10)
+#
+#
+#
+# # В Python не можем директно да конкатенираме число към даден текст
+# a = 5.5
+# b = 3.4
+# result = 'The sum is: ' + str(int(a + b))
+# print(result)
+# result = f'The sum is: {a} + {b}'
+# print(result)
